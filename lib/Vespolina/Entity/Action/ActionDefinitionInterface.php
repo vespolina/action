175e2b1daadc5d58d94cad8437b99b8b9a2deeca
Daniel Kucharski <daniel@xerias.be>
2013-08-14 15:02:24 -0400

    Added entities

---
+++ lib/Vespolina/Entity/Action/ActionDefinitionInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * (c) 2011 - ∞ Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Entity\Action;
+
+/**
+ * An interface defining an action definition
+ *
+ * @author Daniel Kucharski <daniel@xerias.be>
+ */
+interface ActionDefinitionInterface
+{
+    function getName();
+    
+    function getParameters();
+    
+    function getActionHandlerClass();
+    
+    function getSchedulingType();
+    
+    /**
+     * Get the topic of the action definition (eg. order, cart)
+     */
+    function getTopic();
+    
+    function isReprocessingAllowed();
+}
---
+++ lib/Vespolina/Entity/Action/ActionInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * (c) 2011 - ∞ Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Entity\Action;
+
+
+/**
+ * An interface defining an action.
+ *
+ * An action can hold anything, from a notification to a webservice call.
+ * What sets action apart from system events (event dispatcher concept) is the fact that
+ * they can be reprocessed or scheduled to be processed later on (eg. one week later)
+ *
+ * @author Daniel Kucharski <daniel@xerias.be>
+ */
+interface ActionInterface
+{
+    function getName();
+
+    function getSubject();
+
+    function getContext();
+
+}
